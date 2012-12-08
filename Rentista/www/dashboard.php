2011b4447c9833ae5958a495d9b939a7f031d0eb
Chris Amatangelo <chrisamatangelo@gmail.com>
2012-12-08 00:25:17 -0500

    test commit

---
+++ Rentista/www/dashboard.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Rentista</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,600,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<!-- [if IE]>
+	<script src="http://html5shiv.gogglecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+</head>
+<body>
+	<div id="wrap">
+
+		<!-- Main Navigation -->
+		<div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+			  <div class="container">
+			    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			      <span class="icon-bar">1</span>
+			      <span class="icon-bar">2</span>
+			      <span class="icon-bar">3</span>
+			    </a>
+			    <a class="brand" href="#">CitiHabitats Portal</a>
+			    <div class="nav-collapse collapse">
+			      <ul class="nav">
+			        <li class="active"><a href="#">Home</a></li>
+			        <li><a href="#about">About</a></li>
+			        <li><a href="#contact">Contact</a></li>
+			        <li class="dropdown">
+	              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	              <ul class="dropdown-menu">
+	                <li><a href="#">Action</a></li>
+	                <li><a href="#">Another action</a></li>
+	                <li><a href="#">Something else here</a></li>
+	                <li class="divider"></li>
+	                <li class="nav-header">Nav header</li>
+	                <li><a href="#">Separated link</a></li>
+	                <li><a href="#">One more separated link</a></li>
+	              </ul>
+	            </li>
+			      </ul>
+			    </div>
+			  </div>
+			</div>
+		</div>
+
+		<!-- Secondary Navigation -->
+		<div class="navbar second">
+			<div class="cntr_contain">
+				<img src="http://placehold.it/100x100" class="img-circle tenantPhoto">
+			</div>
+		</div>
+
+		<!-- Main Dashboard -->
+		<div id="dash_contain">
+			<!-- Left Column -->
+			<div id="dash_lCol">
+				<div id="dash_labels">
+					<ul>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+				<!-- Tasks -->
+				<ul>
+					<li class="task first">
+						<button id="photoID" data-original-title="blahblahblah" class="btn btn-mini btn-primary" type="button">Mini button</button>
+					</li>
+					<li class="task"></li>
+					<li class="task"></li>
+					<li class="task"></li>
+					<li class="task"></li>
+					<li class="task last"></li>
+				</ul>
+			</div>
+			<!-- Right Column -->
+			<div id="dash_rCol">
+				<div id="prospect" class="rCol">
+					  <div class="progress">
+    					<div class="bar bar-success" style="width: 00%;"></div>
+    				</div>
+				</div>
+				<div id="group" class="rCol">
+				</div>
+			</div>
+			<div class="clear_floats"></div>
+		</div>
+
+		<!-- Footer -->
+		<div id="footer">
+		  <div class="container">
+		    <p class="muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+		  </div>
+		</div>
+
+	</div>
+
+	<!-- Scripts -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/backstretch.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+
+</body>
+</html>
---
+++ Rentista/www/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Rentista</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,600,300' rel='stylesheet' type='text/css'>
+</head>
+	<body>
+		
+		
+		Welcome to Rentista, this is the index page. See I'm editing it.
+		
+		<a href="terms.php">Click here to begin!</a>
+		
+	</body>
+</html>
