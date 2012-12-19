42a768b7e4e8304c927544d297b1df9773382c8d
camatangelo <camatangelo@Chris-Amatangelos-iMac.local>
2012-12-19 07:53:14 -0500

    Fixed directory structure

--- a/php/social_links.php
+++
@@ -1,8 +0,0 @@
-<div class="social_links">
-	<ul>
-		<li><a href="http://twitter.com/WorldnowNews" target="_blank"><img src="../images/twitter_top.png"/></a></li>
-		<li><a href="mailto:info@worldnow.com" target="_blank"><img src="../images/email_top.png"/></a></li>
-		<li><a href="http://www.linkedin.com/company/worldnow" target="_blank"><img src="../images/linked_in_top.png"/></a></li>
-		<li><a href="http://www.facebook.com/WorldNowInc" target="_blank"><img src="../images/facebook_top.png"/></a></li>
-	</ul>
-</div>
